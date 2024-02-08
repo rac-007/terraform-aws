09a4976acb774ca7c872b44a8c126387c6f63b1e
Deven-Rawat <devan.rawat1@nhs.net>
2024-02-08 12:09:43 +0000

    Rachana added variable file

---
+++ vars.tf
@@ -0,0 +1,21 @@
+#Defining Variables
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "instance_name" {
+  default = "Server-Ubuntu-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0c7217cdde317cfec"
+    us-west-1 = "ami-0ce2cb35386fc22e9"
+    eu-west-1 = "ami-0905a3c97561e0b69"
+
+  }
+
+}
